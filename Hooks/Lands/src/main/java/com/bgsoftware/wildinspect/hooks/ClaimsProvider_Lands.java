d8837e51f6ee2e5c07b9d2b379843b9bee15aa5c
nullip <contact@solobyte.xyz>
2024-01-30 12:08:24 -0600

    Updated Lands Integrations

--- Hooks/Lands/src/main/java/com/bgsoftware/wildinspect/hooks/ClaimsProvider_Lands.java
+++ Hooks/Lands/src/main/java/com/bgsoftware/wildinspect/hooks/ClaimsProvider_Lands.java
@@ -1,19 +1,18 @@
 package com.bgsoftware.wildinspect.hooks;
 
 import com.bgsoftware.wildinspect.WildInspectPlugin;
-import me.angeschossen.lands.api.integration.LandsIntegration;
-import me.angeschossen.lands.api.land.LandArea;
-import me.angeschossen.lands.api.role.enums.RoleSetting;
+import me.angeschossen.lands.api.LandsIntegration;
+import me.angeschossen.lands.api.land.Area;
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
+import me.angeschossen.lands.api.flags.type.Flags;
 
 public final class ClaimsProvider_Lands implements ClaimsProvider {
 
-    private final LandsIntegration landsIntegration;
+    private static LandsIntegration landsIntegration;
 
-    public ClaimsProvider_Lands(){
-        landsIntegration = new LandsIntegration(WildInspectPlugin.getPlugin(), false);
-        landsIntegration.initialize();
+    public ClaimsProvider_Lands() {
+        landsIntegration = LandsIntegration.of(WildInspectPlugin.getPlugin());
 
         WildInspectPlugin.log(" - Using Lands as ClaimsProvider.");
     }
@@ -30,8 +29,8 @@ public final class ClaimsProvider_Lands implements ClaimsProvider {
 
     @Override
     public boolean hasRegionAccess(Player player, Location location) {
-        LandArea landArea = landsIntegration.getArea(location);
-        return landArea == null || landArea.canSetting(player.getUniqueId(), RoleSetting.BLOCK_PLACE);
+        Area area = landsIntegration.getArea(location);
+        return area == null || area.hasRoleFlag(player.getUniqueId(), Flags.BLOCK_PLACE);
     }
 
 }
